396218f25c3e16595517178fab9cb3c4708402f9
Sallie McKee <salliemckee@Morrigan.local>
2022-03-22 14:31:48 -0500

    set up redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, compose } from "redux";
+import { Provider } from "react-redux";
 
-import reducer from './reducers';
+import reducer from "./reducers";
 
-import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers());
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
